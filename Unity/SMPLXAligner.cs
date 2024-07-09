704c9129eedda8206336589473322d66622fe8ff
chris-hndz <cris.hndz@gmail.com>
2024-07-09 23:39:25 +0200

    Se actualizó SMPLXAligner para incorporar alineación con joint2D de la pelvis, y creación del quad con la imagen de fondo

--- Unity/SMPLXAligner.cs
+++ Unity/SMPLXAligner.cs
@@ -5,6 +5,7 @@ public class SMPLXAligner : MonoBehaviour
     public string jsonFilePath;
     public GameObject smplxPrefab;
     public Camera alignmentCamera;
+    public Texture2D backgroundImage;
 
     private SMPLXParams parameters;
     private string[] _smplxJointNames = new string[] { "pelvis", "left_hip", "right_hip", "spine1", "left_knee", "right_knee", "spine2", "left_ankle", "right_ankle", "spine3", "left_foot", "right_foot", "neck", "left_collar", "right_collar", "head", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_index1", "left_index2", "left_index3", "left_middle1", "left_middle2", "left_middle3", "left_pinky1", "left_pinky2", "left_pinky3", "left_ring1", "left_ring2", "left_ring3", "left_thumb1", "left_thumb2", "left_thumb3", "right_index1", "right_index2", "right_index3", "right_middle1", "right_middle2", "right_middle3", "right_pinky1", "right_pinky2", "right_pinky3", "right_ring1", "right_ring2", "right_ring3", "right_thumb1", "right_thumb2", "right_thumb3", "jaw"};
@@ -16,7 +17,26 @@ public class SMPLXAligner : MonoBehaviour
         {
             SetupCamera();
             AlignSMPLX();
+
+            // Set up background image
+            SetupBackgroundImage();
         }
+    }
+
+    void SetupBackgroundImage()
+    {
+        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad.transform.position = new Vector3(0, 0, 10); // Ajusta esto según sea necesario
+        
+        float aspectRatio = (float)backgroundImage.width / backgroundImage.height;
+        float quadHeight = 2f * Mathf.Tan(alignmentCamera.fieldOfView * 0.5f * Mathf.Deg2Rad) * quad.transform.position.z;
+        float quadWidth = quadHeight * aspectRatio;
+        
+        quad.transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+        
+        Material mat = new Material(Shader.Find("Unlit/Texture"));
+        mat.mainTexture = backgroundImage;
+        quad.GetComponent<Renderer>().material = mat;
     }
 
     void SetupCamera()
@@ -72,7 +92,10 @@ public class SMPLXAligner : MonoBehaviour
             if (human.shape != null) ApplyShape(smplxComponent, human.shape);
 
             // Adjust pelvis position if needed
-            if (human.translation_pelvis != null) AdjustPelvisPosition(smplxComponent, human.translation_pelvis);
+            if (human.translation_pelvis != null) AdjustPelvisPosition(smplxInstance, human.translation_pelvis);
+
+            // Align with joints2D
+            //if (human.joints_2d != null) AlignWithJoint2D(smplxInstance, human.joints_2d, parameters.image_width, parameters.image_height, human.translation[2]);
         }
     }
 
@@ -144,7 +167,7 @@ public class SMPLXAligner : MonoBehaviour
         Debug.Log("Se han aplicado correctamente las expresiones.");
     }
 
-    void AdjustPelvisPosition(SMPLX smplxMannequin, float[] pelvisTranslation)
+    void AdjustPelvisPosition(GameObject smplxInstance, float[] pelvisTranslation)
     {
         Vector3 pelvisOffset = new Vector3(
             pelvisTranslation[0],
@@ -154,6 +177,19 @@ public class SMPLXAligner : MonoBehaviour
         
         // Adjust the position of the pelvis or the entire model if needed
         // This depends on how your SMPLX model is structured in Unity
-        smplxMannequin.transform.position += pelvisOffset;
+        smplxInstance.transform.position += pelvisOffset;
+    }
+
+    void AlignWithJoint2D(GameObject smplxInstance, float[] joints_2d, float image_width, float image_height, float translationZ)
+    {
+        // Align using pelvis 2D joint
+        Vector2 pelvis2D = new Vector2(joints_2d[0], joints_2d[1]);
+        Vector2 pelvisViewport = new Vector2(pelvis2D.x / image_width, 
+                                             1 - (pelvis2D.y / image_height));
+        Ray ray = alignmentCamera.ViewportPointToRay(new Vector3(pelvisViewport.x, pelvisViewport.y, 0));
+        float pelvisDistance = translationZ;
+        Vector3 pelvis3DWorld = ray.GetPoint(pelvisDistance);
+        Vector3 offset = pelvis3DWorld - smplxInstance.transform.position;
+        smplxInstance.transform.position += offset;
     }
 }
